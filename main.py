19645c2132d859a3ba6cc940e300eb66ed589f86
TheInfoGuru <scates09@gmail.com>
2023-12-09 11:28:49 -0600

    Snake is displaying at start position

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 from turtle import Screen, Turtle
 
+SEGMENT_SIZE = 20
 
-class GameScreen(Screen):
+
+class SnakeSegment(Turtle):
     def __init__(self):
-        super().__init__()
-        super().setup(width=600, height=600)
-        super().bgcolor("black")
-        super().title("My Snake Game")
+        super().__init__(shape="square")
+        self.penup()
+        self.color("white")
 
 
-screen = GameScreen()
+def setup_screen():
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("My Snake Game")
+    return screen
+
+
+def create_snake(snake=None):
+    if snake is None:
+        snake = [SnakeSegment().setx(-20 * i) for i in range(3)]
+    return snake
+
+
+def main():
+    screen = setup_screen()
+    snake = create_snake()
+
+    screen.mainloop()
+
+
+if __name__ == "__main__":
+    main()
